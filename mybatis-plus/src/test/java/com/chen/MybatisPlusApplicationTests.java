f2b1245f54462db879705741f06bf09b1b277040
cyc1998cyc <1812244d>
2023-01-05 22:21:24 +0800

    老笔记本提交

--- mybatis-plus/src/test/java/com/chen/MybatisPlusApplicationTests.java
+++ mybatis-plus/src/test/java/com/chen/MybatisPlusApplicationTests.java
@@ -1,13 +1,29 @@
 package com.chen;
 
+import com.chen.mapper.UserMapper;
+import com.chen.pojo.User;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class MybatisPlusApplicationTests {
+    @Autowired
+    private UserMapper userMapper;
 
     @Test
     void contextLoads() {
+        User user = new User(3, "xiaojiansheng", "1812244934");
+        userMapper.insert(user);
+    }
+
+    @Test
+    void test02(){
+        //查询全部
+        List<User> userList = userMapper.selectList(null);
+        System.out.println(userList);
     }
 
 }
